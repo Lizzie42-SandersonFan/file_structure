0f1cc745d67a82e464608809dab361d69485007d
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-03-18 09:10:44 -0600

    Undo changes

--- src (project name)/main.py
+++ src (project name)/main.py
@@ -1,2 +1,7 @@
-# LD 1st Pet Simulator Personal Project
+# ALL Python files go in the src folder
 
+# main - is required, This is where the project is run from!!!!!!!!!
+#      - Basiclly NOTHING is on this page, it is just used to call other pages with functions that run the program
+#      - Basic UI is allowed on this page BUT it is better to have it on a sparate file
+
+# Only import onto main if at all possible. Do not import multiple files onto your other pages!
