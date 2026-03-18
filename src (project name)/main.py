99132ecddbd51d835a4fd5a95ab59d3a63afb037
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-03-18 09:05:21 -0600

    First commit

--- src (project name)/main.py
+++ src (project name)/main.py
@@ -1,7 +1,2 @@
-# ALL Python files go in the src folder
+# LD 1st Pet Simulator Personal Project
 
-# main - is required, This is where the project is run from!!!!!!!!!
-#      - Basiclly NOTHING is on this page, it is just used to call other pages with functions that run the program
-#      - Basic UI is allowed on this page BUT it is better to have it on a sparate file
-
-# Only import onto main if at all possible. Do not import multiple files onto your other pages!
